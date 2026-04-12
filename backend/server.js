2eb96e489faa29a23b7c41528f08b3be3582ec99
mohammed Qadeer <mr0060195@gmail.com>
2026-04-12 15:23:23 -0600

    production backend url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ["http://localhost:5173", "https://ai-data-dashboard.onrender.com"]
+    origin:"https://ai-data-dashboard.onrender.com"
   }
 ));
 app.use(express.json());
